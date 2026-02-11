59f5ec7c065f6a991c796be4af0ba2df998748dd
Daniel Tekippe <danieltekippe@gmail.com>
2026-02-10 18:43:56 -0600

    Fix: Update NEXT_PUBLIC_API_BASE_URL in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    API_BASE_URL: process.env.API_BASE_URL || 'https://bot-sports-empire.onrender.com/api/v1'
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://bot-sports-empire.onrender.com/api/v1'
   }
 }
 
